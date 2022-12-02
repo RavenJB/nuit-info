175bccd1a0ecdc69f2f7e9d29e3f3e58cf679005
RavenJB <jules.baron26@gmail.com>
2022-12-02 05:24:39 +0100

    nuit-info

--- js/questions.js
+++ js/questions.js
@@ -4,34 +4,49 @@ function questions(numero)
 	switch(numero)
 	{
 		case 1:
-		chaine="Qu’est ce qu’une IST ?*ski*skiing*to skiing*to skying*2";
+		chaine="Qu’est ce qu’une IST ?*Internet Server for Tetris*Infection Sexuellement Transmissible*Хай живе java *Intoxication Sexuellement Transmissible*2";
 		break;
 		case 2:
-		chaine="D’après vous combien de personnes sont touchées par des IST ?*make*do*have*be*3";
+		chaine="D’après vous combien de personnes sont touchées par des IST ?*4850 en 2020*6950 en 2020*2840 en 2020*37 370 en 2020*1";
 		break;
 		case 3:
-		chaine="Comment éviter cela ?*noun*verb*adverb*adjective*4";
+		chaine="Comment éviter cela ?*Avec une bonne douche*Avec des préservatifs*En mettant de la vaseline*En mangeant 5 fruits et légumes par jour*2";
 		break;
 		case 4:
-		chaine="Peut-on attraper une IST en pratiquant une fellation ?*take*make*have*do*1";
+		chaine=" Est-ce qu’un langoureux et sensuel baiser avec échange inter-salivaire peut transmettre une IST ?*Oui*Non***1";
 		break;
 		case 5:
-		chaine="Vous pratiquez vos fellations avec ou sans préservatif ?*come to*come from*come back from*come to from*2";
+		chaine=" Peut-on attraper une IST en pratiquant une fellation ?*Oui*Non***1";
 		break;
 		case 6:
-		chaine="J’ai un micro pénis, comment je fais ?*bring*taken*brought*took*3";
+		chaine="Pour evitez tout risque de transmision, pratiquez-vous vos fellations avec ou sans préservatif ?*Avec*Sans***1";
 		break;
 		case 7:
-		chaine="Peut-on attraper des IST en empruntant le chemin boueux ?*get up*got over*got*got up*4";
+		chaine=" J’ai un micropénis, comment je fais pour les préservatifs ?*Les filles ne sont pas intéressées*Le plus important c’est de savoir l’utiliser*On utilise des préservatifs féminins*Je prends des préservatifs dédié à cette taille*4*3";
 		break;
 		case 8:
-		chaine="Les objets peuvent-ils transmettre des IST ?*asked*said*told*spoke*1";
+		chaine="Peut-on attraper des IST en empruntant le chemin boueux ?*Oui*Non***1";
 		break;
 		case 9:
-		chaine=".*at*to*in*on*3";
+		chaine="Les objets peuvent-ils transmettre des IST ?*Oui*Non***1";
 		break;
 		case 10:
-		chaine=".*althought*but*even*because*4";
+		chaine="D’après vous la prévention en France est-elle suffisante ?*Oui*Non***2";
+		break;
+		case 11:
+		chaine="Quels sont les symptômes du VIH, un troisième bras qui pousse ?*Fièvre et fatigue*Frissons*Mal de tête/gorge*Douleurs articulaires/musculaires*1*2*3*4";
+		break;
+		case 12:
+		chaine="Est-il facile de se faire tester en France ?*Oui*Non***1";
+		break;
+		case 13:
+		chaine="Peut-on vivre avec le VIH ?*Oui*Non***1";
+		break;
+		case 14:
+		chaine=" Comment soigner les IST ?*Avec un Doliprane 1000*Généralement avec des antibiotiques/traitements*Avec un cuite pour oublier*Avec une intervention chirurgicale*2";
+		break;
+		case 15:
+		chaine="Peut-on transmettre des IST autrement qu’avec des rapports sexuels ? *Oui*Non***1";
 		break;
 	}
 	
